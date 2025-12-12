a91504f3eb19d5a40572aff6ebc61cb056ccf428
lookingforexit <lookingforexit@yandex.com>
2025-12-12 21:38:34 +0300

    added MainScreen

---
+++ app/src/main/java/com/example/playlistmaker/MainScreen.kt
@@ -0,0 +1,168 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF3772E7))
+    ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            contentAlignment = Alignment.TopStart
+        ) {
+            Text(
+                modifier = Modifier.padding(start = 12.dp, top = 24.dp),
+                text = "Playlist maker",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+        ScreenContent()
+    }
+}
+
+@Composable
+fun ScreenContent() {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(top = 10.dp)
+        ) {
+            Element(
+                screenIcon = Icons.Default.Search,
+                text = "Поиск",
+                onClick = { }
+            )
+
+            Element(
+                painterIcon = painterResource(R.drawable.ic_music_icon),
+                text = "Плейлист",
+                onClick = {  }
+            )
+
+            Element(
+                screenIcon = Icons.Default.FavoriteBorder,
+                text = "Избранное",
+                onClick = { }
+            )
+
+            Element(
+                screenIcon = Icons.Default.Settings,
+                text = "Настройки",
+                onClick = {  }
+            )
+        }
+    }
+}
+
+@Composable
+fun Element(screenIcon: ImageVector, text: String, onClick: () -> Unit) {
+    TextButton(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(0.dp),
+        shape = RoundedCornerShape(0.dp)
+    ) {
+        ElementContent(
+            icon = { Icon(imageVector = screenIcon, contentDescription = null, tint = Color.Black) },
+            text = text
+        )
+    }
+}
+
+@Composable
+fun Element(painterIcon: Painter, text: String, onClick: () -> Unit) {
+    TextButton(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(0.dp),
+        shape = RoundedCornerShape(0.dp)
+    ) {
+        ElementContent(
+            icon = { Icon(painter = painterIcon, contentDescription = null, tint = Color.Black) },
+            text = text
+        )
+    }
+}
+
+@Composable
+private fun ElementContent(icon: @Composable () -> Unit, text: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            icon()
+            Spacer(modifier = Modifier.padding(horizontal = 10.dp))
+            Text(
+                text = text,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+        }
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowRight,
+            contentDescription = "Перейти",
+            tint = Color.Gray
+        )
+    }
+}
