975d8e2e361ac6555fc8d90afe06ff124b0fef64
lookingforexit <lookingforexit@yandex.com>
2025-12-13 18:48:42 +0300

    added SearchScreen UI

--- app/src/main/java/com/example/playlistmaker/SearchScreen.kt
+++ app/src/main/java/com/example/playlistmaker/SearchScreen.kt
@@ -1,9 +1,107 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.playlistmaker
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun SearchScreen() {
-    Text(text = "Search Screen")
+fun SearchScreen() { var inputText by rememberSaveable { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+
+    ) { TopAppBar(
+        modifier = Modifier,
+        title = {
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = stringResource(R.string.search),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        },
+        navigationIcon = {
+            Icon(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .size(32.dp)
+                    .clickable {},
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Back",
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    )
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 6.dp),
+            value = inputText,
+            onValueChange = { inputText = it },
+            placeholder = {
+                Text(
+                    text = stringResource(R.string.search),
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                )
+            },
+            trailingIcon = {
+                if (inputText.isNotEmpty()) {
+                    Icon(
+                        modifier = Modifier.clickable{ inputText = "" },
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = null
+                    )
+                }
+            },
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.clickable{},
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFFE6E8EB),
+                unfocusedContainerColor = Color(0xFFE6E8EB),
+                focusedTextColor = MaterialTheme.colorScheme.onSurface,
+                unfocusedTextColor = MaterialTheme.colorScheme.onSurface,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedLeadingIconColor = MaterialTheme.colorScheme.onSurface,
+                unfocusedLeadingIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                focusedTrailingIconColor = MaterialTheme.colorScheme.onSurface,
+                unfocusedTrailingIconColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                cursorColor = MaterialTheme.colorScheme.primary
+            ),
+            shape = RoundedCornerShape(size = 8.dp))
+    }
 }
