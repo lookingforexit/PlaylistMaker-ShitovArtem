7e86e5943d0cfa6c44524c080ad120db7da8b901
lookingforexit <lookingforexit@yandex.com>
2026-05-03 23:17:27 +0300

    renamed var to match override method

--- app/src/main/java/com/example/playlistmaker/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/example/playlistmaker/data/network/RetrofitNetworkClient.kt
@@ -6,8 +6,8 @@ import com.example.playlistmaker.data.dto.TracksSearchResponse
 import com.example.playlistmaker.domain.NetworkClient
 
 class RetrofitNetworkClient(private val storage: Storage) : NetworkClient {
-    override fun doRequest(request: Any): TracksSearchResponse {
-        val searchList = storage.search((request as TracksSearchRequest).expression)
+    override fun doRequest(dto: Any): TracksSearchResponse {
+        val searchList = storage.search((dto as TracksSearchRequest).expression)
         return TracksSearchResponse(searchList).apply { resultCode = 200 }
     }
 
