624da5b138969ef2d3429a7525c59e0c48423e84
lookingforexit <lookingforexit@yandex.com>
2026-05-04 13:13:22 +0300

    added interface for playlists repository

---
+++ app/src/main/java/com/example/playlistmaker/domain/PlaylistsRepository.kt
@@ -0,0 +1,18 @@
+package com.example.playlistmaker.domain
+
+import com.example.playlistmaker.data.dto.Playlist
+import com.example.playlistmaker.data.network.Track
+import kotlinx.coroutines.flow.Flow
+
+interface PlaylistsRepository {
+    fun getAllPlaylists(): Flow<List<Playlist>>
+    fun addPlaylist(name: String, description: String)
+    fun getPlaylist(id: Int?): Flow<Playlist?>
+    fun getFavoriteTracks(): Flow<List<Track>>
+    fun insertTrackToPlaylists(track: Track, playlistID: Int)
+
+    suspend fun deletePlaylist()
+    suspend fun deleteTrackFromPlaylist(trackID: Int, playlistID: Int)
+    suspend fun toggleFavorite(track: Track)
+    suspend fun deletePlaylistByID(playlistID: Int)
+}
