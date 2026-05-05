7dcd1591794b7e60f27f3e38602f2a8b9f95bf50
lookingforexit <lookingforexit@yandex.com>
2026-05-05 13:47:28 +0300

    updated data module cause by storage deletion

--- app/src/main/java/com/example/playlistmaker/data/di/DataModule.kt
+++ app/src/main/java/com/example/playlistmaker/data/di/DataModule.kt
@@ -1,7 +1,7 @@
 package com.example.playlistmaker.data.di
 
-import com.example.playlistmaker.creator.Storage
 import com.example.playlistmaker.data.history.SearchHistoryRepositoryImpl
+import com.example.playlistmaker.data.network.ITunesAPIService
 import com.example.playlistmaker.data.network.RetrofitNetworkClient
 import com.example.playlistmaker.data.network.TracksRepositoryImpl
 import com.example.playlistmaker.data.playlist.PlaylistsRepositoryImpl
@@ -11,19 +11,40 @@ import com.example.playlistmaker.domain.TracksRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
     factory<NetworkClient> {
         RetrofitNetworkClient(get())
     }
+    val baseUrl = "https://itunes.apple.com"
 
     factory<TracksRepository> {
         TracksRepositoryImpl(get())
     }
 
+    single<ITunesAPIService> {
+        get<Retrofit>().create(ITunesAPIService::class.java)
+    }
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+    }
+
     single {
-        Storage()
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
     }
 
     single<CoroutineScope> {
