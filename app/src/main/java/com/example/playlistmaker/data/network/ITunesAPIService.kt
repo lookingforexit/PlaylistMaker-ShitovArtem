cf7e0c54139d27670f7cc64e120cc7472f453421
lookingforexit <lookingforexit@yandex.com>
2026-05-05 11:36:53 +0300

    added itunes api service

---
+++ app/src/main/java/com/example/playlistmaker/data/network/ITunesAPIService.kt
@@ -0,0 +1,21 @@
+package com.example.playlistmaker.data.network
+
+import com.example.playlistmaker.data.playlist.TrackSearchByIDResponse
+import com.example.playlistmaker.data.playlist.TracksSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ITunesAPIService {
+    @GET("/search")
+    suspend fun searchTracks(
+        @Query("term") query: String,
+        @Query("media") media: String = "music",
+        @Query("entity") entity: String = "song",
+        @Query("limit") limit: Int = 50
+    ): TracksSearchResponse
+
+    @GET("/lookup")
+    suspend fun lookupTrackById(
+        @Query("id") trackId: Long
+    ): TrackSearchByIDResponse
+}
